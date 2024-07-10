4107cdf8b11994dfe4f6f175762ff21f4ca1f20c
YooYunS <141199486+YooYunS@users.noreply.github.com>
2024-07-10 22:28:30 +0900

    Update gen_anwer.py

--- gen_anwer.py
+++ gen_anwer.py
@@ -1,1 +1,19 @@
+from vllm import LLM, SamplingParams
+from datasets import load_dataset
+import json
+import torch
+import argparse
 
+def args_parse():
+    parser = argparse.ArgumentParser()
+    
+    parser.add_argument("--model_name", type=str)
+    parser.add_argument("--prompt_path", type=str)
+    parser.add_argument("--dataset_path", type=str)
+    parser.add_argument("--benchmark_path", type=str)
+    parser.add_argument("--output_path", type=str, default="output/")
+    
+    return parser.parse_args()
+
+if __name__ == "__main__":
+    args = args_parse()
