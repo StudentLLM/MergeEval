c8bfc299e2a74a1247b4426f440cb8b3dc6910db
YooYunS <141199486+YooYunS@users.noreply.github.com>
2024-07-12 02:12:24 +0900

    Update gen_anwer.py

--- gen_anwer.py
+++ gen_anwer.py
@@ -86,16 +86,18 @@ def get_model_outputs(
             choices.append({"index": i, "outputs": outputs})
         outputs.append(choices)
 
-        os.makedirs(output_path, exist_ok=True)
-        output_dir = "/".join([output_path, model_id, "(", benchmark_type, ")", "_result.json"])
-        with open(output_dir, "a") as fout:
-            ans_json = {
-                "model_id": model_id,
-                "category": question["category"],
-                "content": question["content"],
-                "choices": choices
-            }
-            json.dump(ans_json, fout, intent=4)
+        # os.makedirs(output_path, exist_ok=True)
+        # output_dir = "/".join([output_path, model_id, "(", benchmark_type, ")", "_result.json"])
+        # with open(output_dir, "a") as fout:
+        #     ans_json = {
+        #         "model_id": model_id,
+        #         "category": question["category"],
+        #         "content": question["content"],
+        #         "choices": choices
+        #     }
+        #     json.dump(ans_json, fout, intent=4)
+    
+    return outputs
 
 def run_eval(
     model_name,
@@ -113,49 +115,31 @@ def run_eval(
     questions = load_benchmark(dataset_path, benchmark_type)
     template = load_template(prompt_path, prompt_type)
 
-    llm = LLM(
-        model=model_name, 
-        tensor_parallel_size=torch.cuda.device_count(),
-        trust_remote_code=True,
-        dtype=dtype
+    outputs = get_model_outputs(
+        model_name,
+        model_id,
+        benchmark_type,
+        questions,
+        template,
+        num_choices,
+        dtype,
+        temperature,
+        max_tokens,
+        min_tokens,
+        top_p,
+        repetition_penalty,
+        output_path
     )
+
+    ans_json = [{
+        "model_id": model_id,
+        "category": question["category"],
+        "content": question["content"],
+        "output": outputs[i]
+    } for i, question in enumerate(questions)]
     
-    sampling_params = SamplingParams(
-        temperature=temperature,
-        max_tokens=max_tokens,
-        min_tokens=min_tokens,
-        top_p=top_p,
-        repetition_penalty=repetition_penalty
-    )
-
-    # generate outputs,.....
-    outputs = []
-    for question in tqdm(questions):
-        choices = []
-        for i in range(num_choices):
-            torch.manual_seed(i)
-            prompt = template["system"]
-
-            turns = []
-            for j in range(len(question["turns"]:
-                q = question["turns"][j]
-                prompt += template["input"].format(instruction=q)
-                output = llm.generate(prompt, sampling_params)
-                prompt += output.outputs[0].text
-                turns.append(output)
-            choices.append({"index": i, "outputs": outputs})
-        outputs.append(choices)
-
-        os.makedirs(output_path, exist_ok=True)
-        output_dir = "/".join([output_path, model_id, "(", benchmark_type, ")", "_result.json"])
-        with open(output_dir, "a") as fout:
-            ans_json = {
-                "model_id": model_id,
-                "category": question["category"],
-                "content": question["content"],
-                "choices": choices
-            }
-            json.dump(ans_json, fout, intent=4)
+    with open("/".join([output_path, model_id, "(", benchmark_type, ")", "_result.json"]), "w") as f:
+        json.dump(ans_json, f, indent=4)
 
 if __name__ == "__main__":
     args = args_parse()
